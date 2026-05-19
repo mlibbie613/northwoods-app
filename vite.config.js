ddd5d26f45949c72c2ece196221adb8004a9fc5d
Your GitHub Username <mlibbie613@yahoo.com>
2026-05-18 21:27:28 -0500

    Fix vite config encoding

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
-=======
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> f773a3c43b7290fd0b81a297ab32b2fe16c458bb
